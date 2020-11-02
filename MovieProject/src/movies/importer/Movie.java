4a001fd04222acf19dff25fc6da3f743d794593f
littlett98 <37678140+littlett98@users.noreply.github.com>
2020-11-02 14:28:14 -0500

    Added Movie Class

--- MovieProject/src/movies/importer/Movie.java
+++ MovieProject/src/movies/importer/Movie.java
@@ -1,5 +1,35 @@
 package movies.importer;
 
 public class Movie {
-
+	private String releaseYear;
+	private String movieName;
+	private String runtime;
+	private String source;
+	
+	public Movie(String year, String name, String time, String src) {
+		this.releaseYear = year;
+		this.movieName = name;
+		this.runtime = time;
+		this.source = src;
+	}
+	
+	public String getReleaseYear() {
+		return releaseYear;
+	}
+	
+	public String getMovieName() {
+		return movieName;
+	}
+	
+	public String getRuntime() {
+		return runtime;
+	}
+	
+	public String getSource() {
+		return source;
+	}
+	
+	public String toString() {
+		return getReleaseYear() + "\t" + getMovieName() + "\t" + getRuntime() + "\t" + getSource();
+	}
 }
