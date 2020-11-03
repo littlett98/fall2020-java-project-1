d70515c003abbaabfc828090083fc7fb5e6a7aa1
littlett98 <37678140+littlett98@users.noreply.github.com>
2020-11-03 13:47:21 -0500

    Added a Kaggle Test for the process method and updated the validator to only be for IMDB

--- MovieProject/src/movies/tests/KaggleTests.java
+++ MovieProject/src/movies/tests/KaggleTests.java
@@ -5,7 +5,6 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 
-import junit.framework.Assert;
 import movies.importer.KaggleImporter;
 import java.util.*;
 
