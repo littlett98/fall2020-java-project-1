4ca5b268047cb18ffe9ee8d97fda68b947665c8f
littlett98 <37678140+littlett98@users.noreply.github.com>
2020-11-05 17:06:50 -0500

    Added Javadocs to KaggleImporter and added an exception catch for when there aren't enough columns

--- MovieProject/src/movies/importer/KaggleImporter.java
+++ MovieProject/src/movies/importer/KaggleImporter.java
@@ -1,21 +1,42 @@
 package movies.importer;
 
 import java.util.*;
-
+/**
+ * An extension of the abstract class Processor that imports and formats the kaggle file.
+ * @author Trevor McCubbin
+ */
 public class KaggleImporter extends Processor {
-
+	
+	/** Constructor method to create a new KaggleImporter
+	 * @param source A String representing the source directory
+	 * @param destination A String representing the destination directory
+	 */
 	public KaggleImporter(String source, String destination) {
 		super(source, destination, true);
 	}
 	
+	/**
+	 * Method that processed the imported file from its raw form into it's reduced form
+	 * @param kaggle An ArrayList<String> representing the input from the kaggle file
+	 * @return Returns an ArrayList<String> of the reduced form of the kaggle file 
+	 */
 	public ArrayList<String> process(ArrayList<String> kaggle) {
 		ArrayList<String> newList = new ArrayList<String>();
 		for (int i = 0; i < kaggle.size(); i++) {
 			String[] column = kaggle.get(i).split("\t");
-			if (!(column.length < 19)) {
-				// release Year / title / runtime / source
-				String entry = column[20] + "\t" + column[15] + "\t" + column[13] + "\t" + "kaggle";
-				newList.add(entry);
+			try {
+				if (!(column.length < 19)) {
+					// release Year / title / runtime / source
+					String entry = column[20] + "\t" + column[15] + "\t" + column[13] + "\t" + "kaggle";
+					newList.add(entry);
+				}
+				else {
+					throw new Exception("Not enough columns for an entry on line " + i);
+					
+				}
+			}
+			catch(Exception e) {
+				System.out.println("Not enough columns for a new entry");
 			}
 		}
 		return newList;
