c54bf4cc66165ee35eae83f7e50a95c19c96d5b3
littlett98 <37678140+littlett98@users.noreply.github.com>
2020-11-05 23:22:12 -0500

    Added test for the equals method

--- MovieProject/src/movies/tests/MovieTests.java
+++ MovieProject/src/movies/tests/MovieTests.java
@@ -5,29 +5,45 @@ import movies.importer.Movie;
 
 import org.junit.jupiter.api.Test;
 
+/**
+ * All tests related to the movie class
+ * @author Trevor McCubbin
+ *
+ */
 class MovieTests {
 
 	/** Tests all the get methods within the Movie class
-	 * @author Trevor McCubbin
 	 * 
 	 */
 	@Test
 	void testGetMethods() {
-		Movie m = new Movie("1994", "The Lion King", "1h 58m", "The Internet");
+		Movie m = new Movie("1994", "The Lion King", "158", "The Internet");
 		assertEquals("1994", m.getReleaseYear());
 		assertEquals("The Lion King", m.getName());
-		assertEquals("1h 58m", m.getRuntime());
+		assertEquals("158", m.getRuntime());
 		assertEquals("The Internet", m.getSource());
 	}
 	
 	/** Tests that the toString method returns the desired String in the correct format
-	 * @author Trevor McCubbin
 	 * 
 	 */
 	@Test
 	void testToStringMethod() {
-		Movie m = new Movie("1994", "The Lion King", "1h 58m", "The Internet");
-		assertEquals("1994	The Lion King	1h 58m	The Internet", m.toString());
+		Movie m = new Movie("1994", "The Lion King", "158", "The Internet");
+		assertEquals("1994	The Lion King	158	The Internet", m.toString());
+	}
+	
+	/** Tests the equal method by seeing if it can identify the identical Lion Kings
+	 * vs the new Lion King.
+	 * 
+	 */
+	@Test
+	void testEquals() {
+		Movie m = new Movie("1994", "The Lion King", "158", "The Internet");
+		Movie m2 = new Movie("1994", "The Lion King", "158", "The Internet");
+		Movie m3 = new Movie("2019", "The Lion King", "118", "The Internet");
+		assertEquals(true,m.equals(m2));
+		assertEquals(false, m.equals(m3));
 	}
 
 }
