fa3bb562c9ca33fcba4761195c4ea32d4192b838
littlett98 <37678140+littlett98@users.noreply.github.com>
2020-11-06 11:13:52 -0500

    Changed steps that the normalizer and validator are processed in

--- MovieProject/src/movies/importer/ImportPipeline.java
+++ MovieProject/src/movies/importer/ImportPipeline.java
@@ -11,21 +11,23 @@ public class ImportPipeline {
 	public static void main(String[] args) throws IOException{
 		
 		//Folder that will contain ImdbSmallFile.txt only
-		String imdb_source = "C:\\Users\\Jessie\\Documents\\Dawson\\Computer Science\\java310\\Project 1\\java-project-1\\MovieProject\\files\\imdb_file";
+		String imdb_source = "C:\\Users\\mccub\\Documents\\420-310 (Java 3)\\java-project-1\\MovieProject\\files\\imdb_file";
 		//Folder that will contain KaggleSmallFile.txt only
-		String kaggle_source = "C:\\Users\\Jessie\\Documents\\Dawson\\Computer Science\\java310\\Project 1\\java-project-1\\MovieProject\\files\\kaggle_file";
+		String kaggle_source = "C:\\Users\\mccub\\Documents\\420-310 (Java 3)\\java-project-1\\MovieProject\\files\\kaggle_file";
 		//Folder that will contain processed txt files & preparing to be validated
-		String processed = "C:\\Users\\Jessie\\Documents\\Dawson\\Computer Science\\java310\\Project 1\\java-project-1\\MovieProject\\files\\processed_files";
-		//Folder that will contain validated files and will combine them before going through deduper
-		String validated = "C:\\Users\\Jessie\\Documents\\Dawson\\Computer Science\\java310\\Project 1\\java-project-1\\MovieProject\\files\\validated_files";
+		String processed = "C:\\Users\\mccub\\Documents\\420-310 (Java 3)\\java-project-1\\MovieProject\\files\\processed_files";
 		//Folder that will contain both files combined without duplicates and empty fields
-		String deduped = "C:\\Users\\Jessie\\Documents\\Dawson\\Computer Science\\java310\\Project 1\\java-project-1\\MovieProject\\files\\final_file";
+		String normalized = "C:\\Users\\mccub\\Documents\\420-310 (Java 3)\\java-project-1\\MovieProject\\files\\normalized_file";
+		//Folder that will contain validated files and will combine them before going through deduper
+		String validated = "C:\\Users\\mccub\\Documents\\420-310 (Java 3)\\java-project-1\\MovieProject\\files\\validated_files";
+		//Folder that will contain both files combined without duplicates and empty fields
+		String deduped = "C:\\Users\\mccub\\Documents\\420-310 (Java 3)\\java-project-1\\MovieProject\\files\\final_file";
 		
 		//Creating all the necessary processors
 		ImdbImporter imdb = new ImdbImporter(imdb_source, processed);
-		Validator imdb_v = new Validator(processed, validated);
 		KaggleImporter kaggle = new KaggleImporter(kaggle_source, processed);
-		ValidatorKaggle kaggle_v = new ValidatorKaggle(processed, validated);
+		ValidatorKaggle normalizer= new ValidatorKaggle(processed, normalized);
+		Validator validator = new Validator(normalized, validated);
 		Deduper deduper = new Deduper(validated, deduped);
 		
 		/*
@@ -36,7 +38,7 @@ public class ImportPipeline {
 		 * will join imdb in dest2 to be combined
 		 * Deduper will remove any duplicates
 		 */
-		Processor[] processors = {imdb, imdb_v, kaggle, kaggle_v, deduper};
+		Processor[] processors = {imdb, kaggle, normalizer,  validator, deduper};
 		processAll(processors);
 	}
 	
